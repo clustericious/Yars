0a7fde10dc3505bd6b6703f8300b6410b16845cb
mbrandon <mbrandon@sesda2.com>
2011-03-16 11:53:27 -0400

    version 0.03

--- lib/RESTAS/Yars.pm
+++ lib/RESTAS/Yars.pm
@@ -16,7 +16,7 @@ use strict;
 use warnings;
 use base 'Clustericious::App';
 use RESTAS::Yars::Routes;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 __PACKAGE__->attr( secret => q[rQJzFpwh,ZY;+9dq293.xj6tc?1.oa+a4r/90tCAV] );
 
