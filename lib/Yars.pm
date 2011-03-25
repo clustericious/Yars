3af13e057a52129694ec8f79f75cfcd2412ae144
mbrandon <mbrandon@sesda2.com>
2011-03-25 11:58:29 -0400

    version 0.21

--- lib/Yars.pm
+++ lib/Yars.pm
@@ -10,7 +10,7 @@ use strict;
 use warnings;
 use base 'Clustericious::App';
 use Yars::Routes;
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 __PACKAGE__->attr( secret => q[rQJzFpwh,ZY;+9dq293.xj6tc?1.oa+a4r/90tCAV] );
 
