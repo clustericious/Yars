d7ccd131a901d77e11595bd83d9f5b8939a7aa9e
Brian Duggan <brian.duggan@nasa.gov>
2011-08-31 12:31:39 -0400

    version 0.30 (first release of distributed yars)

--- lib/Yars.pm
+++ lib/Yars.pm
@@ -2,7 +2,7 @@ package Yars;
 
 =head1 NAME
 
-Yars (Yet Another RESTAS Server)
+Yars (Yet Another REST Server)
 
 =cut
 
@@ -11,8 +11,8 @@ use warnings;
 use base 'Clustericious::App';
 use Yars::Routes;
 use Yars::Balancer;
-our $VERSION = '0.21';
+our $VERSION = '0.30';
 
-__PACKAGE__->attr( secret => q[rQJzFpwh,ZY;+9dq293.xj6tc?1.oa+a4r/90tCAV] );
+__PACKAGE__->attr( secret => rand );
 
 1;
