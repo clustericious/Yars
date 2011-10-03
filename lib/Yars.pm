4a380044df045d9419f02f014ab9bb0719368a8b
Brian Duggan <brian.duggan@nasa.gov>
2011-10-03 13:07:22 -0400

    tmpdir test

--- lib/Yars.pm
+++ lib/Yars.pm
@@ -15,4 +15,27 @@ our $VERSION = '0.41';
 
 __PACKAGE__->attr( secret => rand );
 
+package Yars::Message::Request;
+use Mojo::Base 'Mojo::Message::Request';
+has content => sub { Yars::Content::Single->new() };
+
+package Yars::Content::Single;
+use Mojo::Base 'Mojo::Content::Single';
+sub parse {
+    $ENV{MOJO_TMPDIR} = "/tmp";
+    shift->SUPER::parse(@_);
+}
+
+package Yars;
+
+sub startup {
+    my $self = shift;
+    $self->SUPER::startup(@_);
+    $self->hook(after_build_tx => sub {
+        my ($tx,$app) = @_;
+        $tx->req(Yars::Message::Request->new());
+    });
+    1;
+}
+
 1;
