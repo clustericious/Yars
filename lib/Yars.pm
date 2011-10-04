00f55731885c58847c915caedb457ad1b43a3d49
Brian Duggan <brian.duggan@nasa.gov>
2011-10-04 16:28:56 -0400

    version 0.45

--- lib/Yars.pm
+++ lib/Yars.pm
@@ -15,7 +15,7 @@ use Yars::Routes;
 use Yars::Balancer;
 use Yars::Message::Request;
 use Yars::Content::Single;
-our $VERSION = '0.44';
+our $VERSION = '0.45';
 
 __PACKAGE__->attr( secret => rand );
 
