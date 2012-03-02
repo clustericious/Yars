7ac88fad4bfef156349e981007ba73a75663db26
Brian Duggan <brian.duggan@nasa.gov>
2012-03-02 10:07:38 -0500

    version 0.63

--- lib/Yars.pm
+++ lib/Yars.pm
@@ -15,7 +15,7 @@ use Yars::Routes;
 use Yars::Tools;
 use Mojo::ByteStream qw/b/;
 use File::Path qw/mkpath/;
-our $VERSION = '0.62';
+our $VERSION = '0.63';
 
 __PACKAGE__->attr( secret => rand );
 
