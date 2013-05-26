b499ba44a9e33b2c434ac750a8134f6710bd1a7b
Graham Ollis <plicease@cpan.org>
2013-05-25 22:39:12 -0400

    build release 0.83

--- lib/Yars/Routes.pm
+++ lib/Yars/Routes.pm
@@ -1,7 +1,7 @@
 package Yars::Routes;
 
 # ABSTRACT: set up the routes for Yars.
-our $VERSION = '0.82'; # VERSION
+our $VERSION = '0.83'; # VERSION
 
 
 use strict;
@@ -602,7 +602,7 @@ Yars::Routes - set up the routes for Yars.
 
 =head1 VERSION
 
-version 0.82
+version 0.83
 
 =head1 ROUTES
 
