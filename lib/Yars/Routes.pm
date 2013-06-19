fb270adecca78e6df73b4f8e29adb787699b6c13
Graham Ollis <gollis@sesda2.com>
2013-06-18 21:39:24 -0400

    build release 0.83_02

--- lib/Yars/Routes.pm
+++ lib/Yars/Routes.pm
@@ -1,7 +1,7 @@
 package Yars::Routes;
 
 # ABSTRACT: set up the routes for Yars.
-our $VERSION = '0.83_01'; # VERSION
+our $VERSION = '0.83_02'; # VERSION
 
 
 use strict;
@@ -601,7 +601,7 @@ Yars::Routes - set up the routes for Yars.
 
 =head1 VERSION
 
-version 0.83_01
+version 0.83_02
 
 =head1 ROUTES
 
