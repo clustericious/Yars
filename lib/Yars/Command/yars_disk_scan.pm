99c5b880d5cdbd2af130fa00fb2f7f2c71762dc8
Graham Ollis <plicease@cpan.org>
2014-10-06 12:26:51 -0400

    bug 13877: consult lock file for disk scan bug summary: yars_disk_scan processes

--- lib/Yars/Command/yars_disk_scan.pm
+++ lib/Yars/Command/yars_disk_scan.pm
@@ -14,6 +14,17 @@ package Yars::Command::yars_disk_scan;
 Add a cron entry which does a yars_disk_scan periodically
 in order to check the md5s of all the files on disk.
 
+=head1 OPTIONS
+
+=head2 --all | -a
+
+Scan all disks
+
+=head2 --lock filename
+
+Use the given filename as an exclusive lock to ensure that multiple scans are not performed concurrently.
+If there is already a scan in process, the scan will not commence, and instead the process will exit with a 0 return value.
+
 =cut
 
 use strict;
@@ -26,16 +37,19 @@ use File::Find::Rule;
 use Digest::file qw/digest_file_hex/;
 use Getopt::Long qw( GetOptions );
 use Pod::Usage qw( pod2usage );
+use Fcntl ':flock';
 
 sub main {
     #my $class = shift;
     my $status = 0;
     my $conf = Clustericious::Config->new("Yars");
     my $opt_all = 0;
+    my $opt_lock;
     local @ARGV = @_;
     GetOptions(
         'all|a'   => \$opt_all,
         'help|h'  => sub { pod2usage({ -verbose => 2}) },
+        'lock=s'  => \$opt_lock,
         'version' => sub {
             say 'Yars version ', ($Yars::VERSION // 'dev');
             exit 1;
@@ -43,6 +57,17 @@ sub main {
     ) || pod2usage(1);
     my @disks = $opt_all ? (map $_->{root}, map @{ $_->{disks} }, $conf->servers) : (@ARGV);
     LOGDIE "Usage : $0 [-a] [disk1] [disk2] ...\n" unless @disks;
+    
+    if($opt_lock) {
+        use autodie;
+        state $fh;
+        open $fh, '>', $opt_lock;
+        unless(flock $fh, LOCK_EX | LOCK_NB) {
+            INFO "There is already a scan in process...";
+            return 0;
+        }
+    }
+    
     for my $root (@disks) {
         -d $root or next;
         -r $root or next;
