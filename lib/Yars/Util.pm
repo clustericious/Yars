1d735cc458794bb4c128ac76dada784f2d39c9d4
Graham Ollis <perl@wdlabs.com>
2014-12-19 11:23:31 -0500

    doco for util

--- lib/Yars/Util.pm
+++ lib/Yars/Util.pm
@@ -5,7 +5,20 @@ use warnings;
 use 5.010;
 use base qw( Exporter );
 
+# ABSTRACT: Yars internally used functions.
+# VERSION
+
 our @EXPORT_OK = qw( format_tx_error );
+
+=head1 FUNCTIONS
+
+=head2 format_tx_error
+
+ say format_tx_error($tx->error);
+
+Formats a transaction error for human readable diagnostic.
+
+=cut
 
 sub format_tx_error
 {
